624ad40e0bbe9afaf3e4febe3d2feaeeec159818
Алексей Шушунов <shushunov.an@phystech.edu>
2026-08-11 02:22:47 +0300

    использовал новые фнкции для того чтобы отслеживать неправильный ввод

--- first.cpp
+++ first.cpp
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include<stdlib.h>
+#include <ctype.h>
 
 
 #define MAX 3 
 enum coefficents {
-    coeff_a = 0,
-    coeff_b = 1,
-    coeff_c = 2,
+    COEFF_A = 0,
+    COEFF_B = 1,
+    COEFF_C = 2,
 
 };
 
 enum condition {
-    two_sol = 2,
-    one_sol = 1,
-    no_sol = -1,
+    TWO_SOL = 1,
+    ONE_SOL = 0,
+    NO_SOL = -1,
 };
 
 int opening();
 void read_coeff(double *arr);
+bool read_valid_number(double *arr, const char *promt);
 enum condition solve_equation(double *arr,  double *x1, double *x2);
+void print_solution(double x1, double x2, enum condition status);
+
 
 
 int main(){
     double arr[MAX];
-    double root_of_the_solve1, root_of_the_solve2;
-    root_of_the_solve1 = 0;
-    root_of_the_solve2 = 0;
+    double root_solve1, root_solve2;
+    root_solve1 = 0;
+    root_solve2 = 0;
 
     bool want = true ;
 
@@ -36,19 +41,13 @@ int main(){
             printf("задайте коэффиценты квадратного уравнения вида: ax^2 + bx + c = 0\n");
         
             read_coeff(arr);
-            enum condition status =  solve_equation(arr,&root_of_the_solve1, &root_of_the_solve2);
-            if(status == two_sol){
-                printf("корни уравнения: %ls %ls\n",  root_of_the_solve1, root_of_the_solve2);
-            }else if(status == one_sol){
-                printf("корень уравнения: %ls\n", root_of_the_solve1);
-            }else{
-                printf("нет корней\n");
-            }
+            enum condition status =  solve_equation(arr, &root_solve1, &root_solve2);
+            print_solution(root_solve1, root_solve2, status);
         
-        }else{
-            want = false ;
         }
-    }
+        }
+    
+    return 0;
 }
 
 int opening(){
@@ -64,22 +63,16 @@ int opening(){
 }
 
 void read_coeff(double *arr){
-    printf("напишите значение коэффицента перед х^2\n");
-    scanf("%lf", &arr[coeff_a]);
-   
-    printf("напишите значение коэффицента перед х\n");
-    scanf("%lf", &arr[coeff_b]);
-    
-    printf("напишите значение свободного коэффицента \n");
-    scanf("%lf", &arr[coeff_c]);
+     read_valid_number(&arr[COEFF_A], "введите коэффицент перед x^2");
+     read_valid_number(&arr[COEFF_B], "введите коэффицент перед x");
+     read_valid_number(&arr[COEFF_C], "введите свободный коэффицент");
 }
 
 
-
 enum condition solve_equation(double *arr, double *x1, double *x2){
-    double a = arr[coeff_a];
-    double b = arr[coeff_b];
-    double c = arr[coeff_c];
+    double a = arr[COEFF_A];
+    double b = arr[COEFF_B];
+    double c = arr[COEFF_C];
     double D = b*b - 4*a*c;
 
     enum condition result;
@@ -87,17 +80,58 @@ enum condition solve_equation(double *arr, double *x1, double *x2){
         
         *x1 = (-b+sqrt(D))/(2*a);
         *x2 = (-b-sqrt(D))/(2*a);
-        result = two_sol;
+        result = TWO_SOL;
     }else if(D == 0){
         
         *x1 = -b/(2*a);
         *x2 = *x1;
-        result = one_sol;
+        result = ONE_SOL;
     } else{
-        result = no_sol;
+        result = NO_SOL;
     }
     for(int i = 0; i < 3; i++){
         arr[i] = 0; 
     }
     return result;
+}
+
+void print_solution(double x1, double x2, enum condition status){
+            if(status == TWO_SOL){
+                printf("корни уравнения: %lf %lf\n",  x1, x2);
+            }else if(status == ONE_SOL){
+                printf("корень уравнения: %lf\n", x1);
+            }else{
+                printf("нет корней\n");
+            }
+}
+        
+bool read_valid_number(double *value,  const char *promt){
+    char buffer[100] = {0};
+    char *point_to_the_end; 
+    double number = 0;
+    while(true){
+        printf("%s", promt);
+        fgets(buffer, sizeof(buffer), stdin);
+
+        if(buffer[0] == '\0' || buffer[0] == '\n'){
+            printf("пустой ввод\n");
+            continue;
+        }
+
+        number = strtod(buffer, &point_to_the_end);
+
+        while(*point_to_the_end != '\0' && *point_to_the_end != '\n'){
+            if(!isspace(*point_to_the_end)){
+                printf("ошибка ввода\n");
+                break;
+            }
+            point_to_the_end++;
+            }
+        if(*point_to_the_end != '\0' || *point_to_the_end != '\n'){
+            
+            continue; 
+        }
+}
+    *value = number;
+    return true;
 }
