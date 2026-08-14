9afbef8396c348c644bae8c6afdebd3a0dd80729
Алексей Шушунов <shushunov.an@phystech.edu>
2026-08-14 16:28:55 +0300

    доработки с неймингами и возможностью решать линейные уравнения

--- equation.h
+++ equation.h
@@ -3,23 +3,24 @@
 
 
 enum coefficents {
-    COEFF_A = 0,
-    COEFF_B = 1,
-    COEFF_C = 2,
+    COEFF_A_INDEX = 0,
+    COEFF_B_INDEX = 1,
+    COEFF_C_INDEX = 2,
 
 };
 
 enum condition {
     TWO_SOL = 2,
     ONE_SOL = 1,
-    NO_SOL = -1,
+    NO_SOL = 0,
     correct_inp = 3,
-    incorrect_inp = 0
+    incorrect_inp = -1
 };
 
 int programm_opening();
 enum condition read_coeff(double *arr);
 enum condition read_valid_number(double *arr, const char *promt);
-enum condition solve_equation(double *arr,  double *x1, double *x2);
+enum condition solve_quadratic_equation(double *arr,  double *x1, double *x2);
 void print_solution(double x1, double x2, enum condition status);
+enum condition solve_equation(double *arr_variables, double *x1, double *x2);
 #endif
--- first.cpp
+++ first.cpp
@@ -11,7 +11,7 @@
 
 
 int main(){
-    double arr[3] = {0};
+    double arr_variables[3] = {0};
     double root_solve1, root_solve2;
     root_solve1 = 0;
     root_solve2 = 0;
@@ -19,12 +19,18 @@ int main(){
     bool want_to_solve = programm_opening() ;
 
     while(want_to_solve){
-        printf("задайте коэффиценты квадратного уравнения вида: ax^2 + bx + c = 0\n");
+        printf("задайте коэффиценты квадратного уравнения вида: ax^2 + bx + coeff_c = 0\n");
         
-        enum condition status = read_coeff(arr);
+        enum condition status_vr = read_coeff(arr_variables);
+        enum condition status;
 
-        if(status == correct_inp){
-            status = solve_equation(arr, &root_solve1, &root_solve2);
+        if(status_vr == correct_inp){
+            if(arr_variables[COEFF_A_INDEX] != 0){
+                status = solve_quadratic_equation(arr_variables, &root_solve1, &root_solve2);
+            }else{
+                printf("уравнение не квадратное\n");
+                status = solve_equation(arr_variables, &root_solve1, &root_solve2);
+            }
         }
         
         print_solution(root_solve1, root_solve2, status);
@@ -38,7 +44,7 @@ int main(){
 int programm_opening(){
     char user_choice = 0;
     printf("хотите решить квадратное уравнение? (y/n)\n");
-    scanf(" %c", &user_choice);
+    scanf(" %coeff_c", &user_choice);
 
     while(getchar() != '\n');
     
@@ -49,7 +55,7 @@ int programm_opening(){
     }
 }
 
-enum condition read_coeff(double *arr){
+enum condition read_coeff(double *arr_variables){
     const char *promts[3] = {
         "введите коэффицент перед x^2",
         "введите коэффицент перед x",
@@ -57,51 +63,59 @@ enum condition read_coeff(double *arr){
     };
     int status_in_proc;
     for(int i = 0; i < 3; ++i){
-        enum condition status_in_proc = read_valid_number(&arr[i], promts[i]);
+        enum condition status_in_proc = read_valid_number(&arr_variables[i], promts[i]);
         if(status_in_proc == incorrect_inp){
-        return status_in_proc; 
+            return status_in_proc; 
         }
     }
     return correct_inp;
 }
 
 
-enum condition solve_equation(double *arr, double *x1, double *x2){
-    double a = arr[COEFF_A];
-    double b = arr[COEFF_B];
-    double c = arr[COEFF_C];
-    double D = b*b - 4*a*c;
+enum condition solve_quadratic_equation(double *arr_variables, double *x1, double *x2){
+    double coeff_a = arr_variables[COEFF_A_INDEX];
+    double coeff_b = arr_variables[COEFF_B_INDEX];
+    double coeff_c = arr_variables[COEFF_C_INDEX];
+    double D = coeff_b*coeff_b - 4*coeff_a*coeff_c;
 
     enum condition result;
     if (D > 0 ){
         
-        *x1 = (-b+sqrt(D))/(2*a);
-        *x2 = (-b-sqrt(D))/(2*a);
+        *x1 = (-coeff_b+sqrt(D))/(2*coeff_a);
+        *x2 = (-coeff_b-sqrt(D))/(2*coeff_a);
         result = TWO_SOL;
     }else if(D == 0){
         
-        *x1 = -b/(2*a);
+        *x1 = -coeff_b/(2*coeff_a);
         *x2 = *x1;
         result = ONE_SOL;
     } else{
         result = NO_SOL;
     }
     for(int i = 0; i < 3; i++){
-        arr[i] = 0; 
+        arr_variables[i] = 0; 
     }
     return result;
 }
 
 void print_solution(double x1, double x2, enum condition status){
-        if(status == TWO_SOL){
+    switch(status){
+        case  TWO_SOL:
             printf("корни уравнения: %lf %lf\n",  x1, x2);
-        }else if(status == ONE_SOL){
+            break;
+        case ONE_SOL:
             printf("корень уравнения: %lf\n", x1);
-        }else if(status == incorrect_inp){
+            break;
+        case incorrect_inp:
             printf("некорректный ввод\n");
-        }else{
+            break;
+        case NO_SOL:
             printf("нет корней\n");
-        }
+            break;
+        default:
+            printf("неизвестный статус\n");
+            break;
+    }
 }
 
         
@@ -109,7 +123,8 @@ enum condition read_valid_number(double *value,  const char *promt){
     printf("%s\n", promt);
     double number = 0;
     char buffer[100] = {0};
-    char *endword;
+    char *endword = 0;
+
     fgets(buffer, sizeof(buffer), stdin);
     if(buffer[0] == '\n' || buffer[0] == '\0'){
         printf("пустой ввод\n");
@@ -124,6 +139,15 @@ enum condition read_valid_number(double *value,  const char *promt){
         *value = number;
         return correct_inp;
     }
+}
 
-
+enum condition solve_equation(double *arr_variables, double *x1, double *x2){
+    double coeff_b = arr_variables[COEFF_B_INDEX];
+    double coeff_c = arr_variables[COEFF_C_INDEX];
+    if(coeff_b == 0){
+        return NO_SOL;
+    }else{
+        *x1 = *x2 = (-coeff_c)/coeff_b ;
+        return ONE_SOL;
+    }
 }
